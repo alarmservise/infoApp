b5e5d2d133aedc423e702eed3913dd74fe5c4dd6
Alarm <alarmservise@mail.ru>
2023-07-15 15:19:46 +0300

    1 урок, но нужно разобраться с меню

--- app/src/main/java/com/example/infoapp/MainActivity.kt
+++ app/src/main/java/com/example/infoapp/MainActivity.kt
@@ -1,46 +1,46 @@
 package com.example.infoapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material.*
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import com.example.infoapp.ui.theme.InfoAppTheme
+import com.example.infoapp.ui_components.DrawerMenu
+import com.example.infoapp.ui_components.MainTopBar
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter",
+        "UnusedMaterial3ScaffoldPaddingParameter"
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val scaffoldState = rememberScaffoldState()
+            val topBarTitle = remember {
+                mutableStateOf("Грибы")
+            }
             InfoAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                Scaffold(
+                    scaffoldState = scaffoldState,
+                    topBar = {
+                        MainTopBar(
+                            title = topBarTitle.value,
+                            scaffoldState
+                        )
+                    },
+                    drawerContent = {
+                        DrawerMenu()
+                    }
                 ) {
-                    Greeting("Android123")
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    InfoAppTheme {
-        Greeting("Android")
-    }
-}
+
